037ed1976783e5324682903a5f2ddb14b5e586b4
artemiy <artemiy.shuppa@gmail.com>
2025-10-02 02:04:42 +0500

    feat(main.py) add basic pipeline

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
-import json
+from datetime import datetime
+from formatter.MarkdownFormatter import MarkdownFormatter
 
+from dotenv import load_dotenv
+
+from analyzer.TechFrequencyAnalyzer import TechFrequencyAnalyzer
 from collector.HeadHunterCollector import HeadHunterCollector
+from model.report_model import ReportContent, Title
+from sender.telegram_sender import TelegramSender
 
 
 def main():
+    load_dotenv()
+
     try:
         collector = HeadHunterCollector()
         vacancies = collector.collect("DevOps")
-        print(json.dumps(vacancies, ensure_ascii=False, indent=4))
+
+        reports = []
+        date_time = datetime.now()
+        reports.append(Title(f"Report for {date_time}"))
+
+        analyzer = TechFrequencyAnalyzer()
+        reports.append(Title("TechFrequencyAnalyzer", level=2))
+        reports.extend(analyzer.analyze(vacancies))
+
+        formatter = MarkdownFormatter()
+        final_result = formatter.format_report(ReportContent(reports))
+
+        sender = TelegramSender()
+        sender.send_message(final_result)
 
     except Exception as e:
         print(f"Something goes wrong: {e}")
