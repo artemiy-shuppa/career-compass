21431c580423d13aee4c94dcf9799b1f60cd8589
artemiy <artemiy.shuppa@gmail.com>
2025-10-07 12:14:54 +0500

    feat(main.py): add notification on error

--- main.py
+++ main.py
@@ -31,7 +31,30 @@ def main():
         sender.send_message(final_result)
 
     except Exception as e:
-        print(f"Something goes wrong: {e}")
+        try:
+            error_message = format_error_message(e)
+            sender = TelegramSender()
+            sender.send_message(error_message)
+
+        except Exception as send_error:
+            print("Critical Error: can't send notification to telegram")
+            print(f"Initial Error: {e!r}")
+            print(f"Error sending to telegram: {send_error!r}")
+
+
+def format_error_message(e: Exception):
+    error_type = type(e).__name__
+    error_message = str(e)
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    final_message = [
+        "**Error in Career Compass pipeline**\n",
+        f"**Time** `{timestamp}`",
+        f"**Error type**: `{error_type}`",
+        "**Message**:\n```",
+        f"{error_message}```",
+    ]
+    return "\n".join(final_message)
 
 
 if __name__ == "__main__":
